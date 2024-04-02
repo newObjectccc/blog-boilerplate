1ee3a2a1db31ab0e9120fffd766b8beac066c3c8
newObjectccc <897333949@qq.com>
2024-04-02 18:07:18 +0800

    feat: page update

---
+++ src/app/lify/page.tsx
@@ -0,0 +1,8 @@
+export default function Lify() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold">Lify</h1>
+      <p className="text-lg">Articles about programming and life</p>
+    </main>
+  );
+}
---
+++ src/app/workspace/page.tsx
@@ -0,0 +1,8 @@
+export default function Workspace() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-3xl font-bold">Workspace</h1>
+      <p className="text-lg">Articles about programming and life</p>
+    </main>
+  );
+}
